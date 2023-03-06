05aecfdf029d2df6c9031dc09678328d34cff481
Sandhya Rani <sandhya.12012002@gmail.com>
2023-03-06 20:30:11 +0530

    Created Diesel Engine class

---
+++ DieselEngine.java
@@ -0,0 +1,7 @@
+public class DieselEngine{
+    public static void main(String[] agrs){
+        public void start(){
+            System.out.print("Diesel Engine Started");
+        }
+    }
+}
--- vehicle.java
+++ vehicle.java
@@ -2,5 +2,8 @@ public class vehicle{
     public static void main(String[] args){
         PetrolEngine pE = new PetrolEngine();
         pE.start();
+
+        DieselEngine dE = new DieselEngine();
+        dE.start();
     }
 } 
