c1cdccaef7b08937d363de811f7b14b7fa22a4df
Sandhya Rani <sandhya.12012002@gmail.com>
2023-03-07 19:58:30 +0530

    hydrogen engine added

---
+++ HydrogenEngine.java
@@ -0,0 +1,8 @@
+public class HydrogenEngine{
+    public void start(){
+        System.out>print("Hydrogen Engine Started");
+    }
+    public void stop(){
+            System.out.print("Hydrogen Engine Stopped");
+        }
+}
